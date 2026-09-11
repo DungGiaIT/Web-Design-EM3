4b969e61f51ff410826dd2979e13a92068cbc383
DungGiaIT <dungdkso1@gmail.com>
2026-09-12 05:00:53 +0700

    BTVN

--- lab1/9.modules/lab.js
+++ lab1/9.modules/lab.js
@@ -1,4 +1,8 @@
 import{greet} from './module.js';
+import { squareArea } from './module.js';   
+let  h = 10;
+let w = 10;
 
+console.log(squareArea(w,h));
 const name = "Alice";
 console.log(greet(name)); // Output: Hello, Alice!
--- lab1/9.modules/module.js
+++ lab1/9.modules/module.js
@@ -1,1 +1,5 @@
 export const greet = (name) => `Hello, ${name}!`;
+
+export function squareArea(h,w){
+    return h*w;
+}
